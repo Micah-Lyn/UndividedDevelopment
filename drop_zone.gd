ca46146639c2e3049260f678bcdffc085f3d7fe3
hawkidav000 <david.james@hawkinsonline.us>
2022-03-28 13:26:11 -0500

    Changed the instruction label and added circles.

--- drop_zone.gd
+++ drop_zone.gd
@@ -2,7 +2,8 @@ extends Position2D
 
 
 func _draw():
-	draw_circle(Vector2.ZERO, 20, Color.transparent)
+	# draw_circle(Vector2.ZERO, 20, Color.transparent)
+	draw_arc(Vector2.ZERO, 15, deg2rad(0), deg2rad(360), 100, Color.whitesmoke, 5)
 	
 func select():
 	for child in get_tree().get_nodes_in_group("zone"):
