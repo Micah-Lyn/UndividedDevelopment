cedb2e114c54c3202382519243d94e282d6e7683
hawkidav000 <david.james@hawkinsonline.us>
2022-03-29 22:42:11 -0500

    Added drag and drop for Area problem.

--- area_drop_zone.gd
+++ area_drop_zone.gd
@@ -2,8 +2,8 @@ extends Position2D
 
 var fill = false
 
-func _draw():
-	draw_rect(Rect2(Vector2(-50, -50), Vector2(100, 100)), Color.red, false, 5)
+#func _draw():
+#	draw_rect(Rect2(Vector2(-50, -50), Vector2(100, 100)), Color.red, false, 5)
 	
 func select():
 	fill = true
