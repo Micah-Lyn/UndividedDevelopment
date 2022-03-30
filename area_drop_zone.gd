3b9e9e180461480917be72fe0f7ffa24740d81e0
natevan <nathanvandevyvere@gmail.com>
2022-03-29 20:25:13 -0500

    updatated area scripts to disable sprites

--- area_drop_zone.gd
+++ area_drop_zone.gd
@@ -1,13 +1,9 @@
 extends Position2D
 
+var fill = false
 
 func _draw():
 	draw_rect(Rect2(Vector2(-50, -50), Vector2(100, 100)), Color.red, false, 5)
 	
 func select():
-	for child in get_tree().get_nodes_in_group("zone"):
-		child.deselect()
-	modulate = Color.webmaroon
-	
-func deselect():
-	modulate = Color.whitesmoke
+	fill = true
