43385193becb8af0bb7f408f8fd5c65556a9dff6
hawkidav000 <david.james@hawkinsonline.us>
2022-04-12 14:56:30 -0500

    Playing with the back buttons

--- src/StartUpScreen/EnterButton.gd
+++ src/StartUpScreen/EnterButton.gd
@@ -12,6 +12,7 @@ export(bool) var start_focused = false
 onready var _cwd = $CorrectWindowDialog
 onready var _iwd = $IncorrectWindowDialog
 onready var _nawd = $NoAttemptsWindowDialog
+onready var _back = get_parent().get_node("BackButton")
 
 onready var _le = get_parent().get_node("UserAnswerLineEdit")
 
@@ -39,12 +40,14 @@ func _on_Button_Pressed():
 	if _le.text == "21":
 		_cwd.popup_centered()
 		self.disabled = true
+		_back.disabled = false
 	else:
 		if numAttempts < 2:
 			_iwd.popup_centered()
 		else:
 			_nawd.popup_centered()
 			self.disabled = true
+			_back.disabled = false
 	print("Enter button was pressed")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
